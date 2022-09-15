f2dd4ab389e7a86653c6add55e49305ac4bb706a
Maham Khan <mahamimranamjad@gmail.com>
2022-09-15 11:06:31 -0700

    added about

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+// import Header from './components/Header';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          myportfolio
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+        
+      </header> */}
+      <main>
+      <About/>
+      {/* <Header/>
+      <Footer/>
+      <Project/>
+      <Skills/>
+      <Contact/>
+      <Nav/> */}
+      </main>
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+
+
+
+const About = ()=> {
+    return(
+        <div>
+            <h1>About</h1>
+            <h4>I am a Full-stack web developer with a background in Biotechnology.</h4>
+        </div>
+        
+    )
+}
+
+export default About
