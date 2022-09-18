4380e8b9c3b68def171ae8572b8c2dbd0f9cddc9
Maham Khan <mahamimranamjad@gmail.com>
2022-09-18 09:33:01 -0700

    added projects and skills

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,25 @@
 import React from "react";
-
+import project1 from '../media/project1.webm'
+import project2 from '../media/project2.webm'
+import project3 from '../media/project3.webm'
+import project4 from '../media/project4.webm'
 
 export default function Project(){
     return(
+        <div>
         <h1>Projects</h1>
+         <video autoPlay width="250">
+            <source src={project1}/>
+        </video>
+        <video autoPlay width="250">
+            <source src={project2}/>
+        </video> 
+        <video autoPlay width="250">
+            <source src={project3}/>
+        </video> 
+        <video autoPlay width="250">
+            <source src={project4}/>
+        </video> 
+        </div>
     )
 }
