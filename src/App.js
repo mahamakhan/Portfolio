dee7a17f51a6400910bb637994ee722bd2f5076b
mahamakhan <mahamimranamjad@gmail.com>
2022-09-19 13:47:46 -0700

    added navlinks

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import './App.css';
-import About from './components/About';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import About from './components/About'
 import Project from './components/Project'
 import Skills from './components/Skills'
 import Nav from './components/Nav'
 import Contact from './components/Contact'
-// import Header from './components/Header';
-
-
+import Resume from './components/Resume'
 
 function App() {
   return (
     <div className="App">
       <header className="header">
-        <Nav/> 
-      </header> 
+        <Nav />
+      </header>
       <main>
-      <About/>
-      {/* <Header/>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+        <About />
+        {/* <Header/>
       <Footer/> */}
-      <Project/>
-      <Skills/>
-      <footer className="footer">
-  <Contact/> 
-      </footer >
-      
-      
+        <Project />
+        <Skills />
+        <footer className="footer">
+          <Resume />
+          <Contact />
+        </footer>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-
+import { NavLink } from 'react-router-dom'
 
 export default function Nav(){
     return(
+        <div>
         <h1>Nav</h1>
+        <div>
+            <NavLink to='/'>About</NavLink>
+            <NavLink to='/projects'>Projects</NavLink>
+            <NavLink to='/skills'>Skills</NavLink>
+            <NavLink to='/resume'>Resume</NavLink>
+            <NavLink to='/contact'>Contact</NavLink>
+        </div>
+        </div>
+
     )
 }
