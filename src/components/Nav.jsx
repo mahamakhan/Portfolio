2785ab6546a781a2d920cfa92576e89ae14a0cc6
mahamakhan <mahamimranamjad@gmail.com>
2022-09-22 11:05:22 -0700

    font

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,9 +12,9 @@ export default function Nav(){
         <div className="navbar">
         <div>
             <ul >
-            <li className="nav"><Link to='aboutpage' spy={true} smooth={true} offset={-150} duration={500}><SiAboutdotme color={"grey"}/></Link></li>
-            <li className="nav"><Link to='projectpage' spy={true} smooth={true} offset={-60} duration={500}><FaCode color={"grey"}/></Link></li>
-            <li className="nav"><Link to='skillspage' spy={true} smooth={true} offset={-100} duration={500}><GiSkills color={"grey"}/></Link></li>
+            <li className="nav"><Link to='aboutpage' spy={true} smooth={true} offset={-95} duration={500}><SiAboutdotme color={"grey"}/></Link></li>
+            <li className="nav"><Link to='projectpage' spy={true} smooth={true} offset={-50} duration={500}><FaCode color={"grey"}/></Link></li>
+            <li className="nav"><Link to='skillspage' spy={true} smooth={true} offset={-40} duration={500}><GiSkills color={"grey"}/></Link></li>
             <li className="nav"><Link to='resumepage' spy={true} smooth={true} offset={-100} duration={500}><HiOutlineDocumentText color="grey"/></Link></li>
             <li className="nav"><Link to='contactpage' spy={true} smooth={true} offset={-100} duration={500}><MdContactPhone color="grey"/></Link></li>
             </ul>
