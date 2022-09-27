7df821b57440339f0bf49fa7f2bebd9b34c7b602
mahamakhan <mahamimranamjad@gmail.com>
2022-09-27 11:49:38 -0700

    projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,36 +13,41 @@ export default function Project(){
         <h1>Projects</h1>
         <div className="projects">
         <div className="projcard">
-         <video autoPlay width="300">
+         <video autoPlay width="300" height="300">
             <source src={project1}/>
         </video>
         <figcaption className="show">
         <h2>Memory Game</h2>
+        <h4>Memory game which includes creating matched pairs.</h4>
         <SiCss3/>
         <SiHtml5/>
         <TbBrandJavascript/>
         <a href="https://github.com/mahamakhan/Memory-Game"><SiGithub className="github" color={"grey"}/></a>
+        
         </figcaption>
         </div>
         <div className="projcard">
-        <video autoPlay width="300">
+        <video autoPlay width="300" height="300">
             <source src={project2}/>
         </video> 
         <figcaption className="show">
         <h2>Travel Itinerary</h2>
+        <h4>A travel app to log your destinations and update them. There is also an option to make a wish list.</h4>
         <SiNodedotjs/>
         <SiExpress/>
         <SiMongodb/>
         <SiReact/>
         <a href="https://github.com/mahamakhan/Travel-Itinerary"><SiGithub className="github" color={"grey"}/></a>
+        
         </figcaption>
         </div>
         <div className="projcard">
-        <video autoPlay width="300">
+        <video autoPlay width="300" height="300">
             <source src={project3}/>
         </video> 
         <figcaption className="show">
         <h2>Group Finder</h2>
+        <h4>An app allowing the user to sign up, create groups, and join already created groups to play games online.</h4>
         <SiNodedotjs/>
         <SiExpress/>
         <SiPostgresql/>
@@ -53,11 +58,12 @@ export default function Project(){
         </figcaption>
         </div>
         <div className="projcard">
-        <video autoPlay width="300">
+        <video autoPlay width="300" height="300">
             <source src={project4}/>
         </video> 
         <figcaption className="show">
         <h2>NGOs-Give Charity</h2>
+        <h4>An app with details of different NGOs and locations to donate items added using an API.</h4>
         <SiPython/>
         <SiDjango/>
         <RiVuejsFill/>
