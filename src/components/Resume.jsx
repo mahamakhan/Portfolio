c4337852070e46b2d590621367871f7cf39b2f59
mahamakhan <mahamimranamjad@gmail.com>
2022-09-27 16:16:50 -0700

    resumepage

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,10 +4,13 @@ import {VscFilePdf} from 'react-icons/vsc'
 import {TiDocumentText} from 'react-icons/ti'
 export default function Resume(){
     return(
-        <div className="resumepage">
+        <div>
         <h1>Resume</h1>
-        <a href={resume} download="Maham Khan Resume"><VscFilePdf className="icons" size="4em" color="lightgrey"/></a>
-        <a href="https://docs.google.com/document/d/1f6dTm1sdvKy0NqQbcGSsP5WNWAJzaMKHvzFglJM4nDw/edit?usp=sharing"><TiDocumentText className="icons" size="4em" color="lightgrey"/></a>
+        <div className="resumepage">
+        <h2>Download:</h2><a href={resume} download="Maham Khan Resume"><VscFilePdf className="icons" size="4em" color="lightgrey"/></a>
+
+        <h2>Open:</h2><a href="https://docs.google.com/document/d/1f6dTm1sdvKy0NqQbcGSsP5WNWAJzaMKHvzFglJM4nDw/edit?usp=sharing"><TiDocumentText className="icons" size="4em" color="lightgrey"/></a>
+        </div>
         </div>
     )
 }
