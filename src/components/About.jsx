c28c9121ac872255c282b7ca24861cf1e6f71949
mahamakhan <mahamimranamjad@gmail.com>
2022-09-27 16:06:46 -0700

    skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,8 @@ const About = ()=> {
             
             <div className='about'>
             <img className='mypic' src={picture} alt='mypic'/>
-            <h3>I am from Pakistan and I moved to the United States in 2020. I got my Bachelors of Science degree in Biotechnology. I am a Full-stack web developer with a background in Biotechnology and healthcare, focused on improving the world through the use of new technologies. I am hard working, detail-oriented, and persistent. I frequently challenge myself to improve my skill-set and am driven to contribute with a company that I can grow with long-term.
+            <h3>I am from Pakistan and I moved to the United States in 2020. I got my Bachelors of Science degree in Biotechnology from Forman Christian College, Lahore, Pakistan. Working in a fully automated diagnostic lab developed my interest towards learning coding. My goal is focused on improving the world through the use of new technologies. I am hard working, detail-oriented, and persistent. I frequently challenge myself to improve my skill-set and am driven to contribute with a company that I can grow with long-term.
+           <br/>
            Apart from coding I like hiking, playing tennis, painting and spending time with my family. </h3>
         </div>
         </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,36 +8,66 @@ export default function Skills(){
             <h1>Skills</h1>
         
         <div className="skills">
+            <div>
         <RiVuejsFill className="icons" size= '3em'/>
-        <h5>VueJs</h5>
+        <h3>VueJs</h3>
+        </div>
+        <div>
         <SiDjango className="icons" size= '3em'/>
-        <h5>Django</h5>
+        <h3>Django</h3>
+        </div>
+        <div>
         <SiHtml5 className="icons" size= '3em'/>
-        <h5>HTML</h5>
+        <h3>HTML</h3>
+        </div>
+        <div>
         <SiTailwindcss className="icons" size= '3em'/>
-        <h5>Tailwind</h5>
+        <h3>Tailwind</h3>
+        </div>
+        <div>
         <SiCss3 className="icons" size= '3em'/>
-        <h5>CSS</h5>
+        <h3>CSS</h3>
+        </div>
+        <div>
         <SiReact className="icons" size= '3em'/>
-        <h5>React</h5>
+        <h3>React</h3>
+        </div>
+        <div>
         <TbBrandJavascript className="icons" size= '3em'/>
-        <h5>Javascript</h5>
+        <h3>Javascript</h3>
+        </div>
+        <div>
         <SiNodedotjs className="icons" size= '3em'/>
-        <h5>Node.js</h5>
+        <h3>Node.js</h3>
+        </div>
+        <div>
         <SiExpress className="icons" size= '3em'/>
-        <h5>Express</h5>
+        <h3>Express</h3>
+        </div>
+        <div>
         <SiMongodb className="icons" size= '3em'/>
-        <h5>MongoDb</h5>
+        <h3>MongoDb</h3>
+        </div>
+        <div>
         <SiPython className="icons" size= '3em'/>
-        <h5>Python</h5>
+        <h3>Python</h3>
+        </div>
+        <div>
         <SiPostgresql className="icons" size= '3em'/>
-        <h5>Postgresql</h5>
+        <h3>Postgresql</h3>
+        </div>
+        <div>
         <SiSequelize className="icons" size= '3em'/>
-        <h5>Sequelize</h5>
+        <h3>Sequelize</h3>
+        </div>
+        <div>
         <SiHeroku className="icons" size= '3em'/>
-        <h5>Heroku</h5>
+        <h3>Heroku</h3>
+        </div>
+        <div>
         <SiGithub className="icons" size= '3em'/>
-        <h5>Gtihub</h5>
+        <h3>Gtihub</h3>
+        </div>
         </div>
         </div>
     )
